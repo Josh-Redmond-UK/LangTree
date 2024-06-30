5eb5ee629106ed27130904380fc6d423d763c6a1
Josh Redmond <jr725@exeter.ac.uk>
2024-06-30 21:48:01 +0100

    removed all vars in a dumb plan

--- api/main.py
+++ api/main.py
@@ -10,15 +10,16 @@ from fastapi.middleware import Middleware
 
 
 #default globals
-global tokenizer_string
-global model_string
-global tokenizer
-global model
-tokenizer_string = 'openai-community/gpt2'
-model_string = 'openai-community/gpt2'
-tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_string)
-model = GPT2LMHeadModel.from_pretrained(model_string)
-attribution_model = inseq.load_model(model, "saliency")
+#global tokenizer_string
+#global model_string#
+#global #tokenizer
+#global model
+#tokenizer_string = 'openai-community/gpt2'
+#model_string = 'openai-community/gpt2'
+#tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_string)
+#model = GPT2LMHeadModel.from_pretrained(model_string)
+#attribution_model = inseq.load_model(model, "saliency")
+#valid_params = {'models':['openai-community/gpt2'], 'tokenizers':['openai-community/gpt2']}
 
 
 
@@ -39,7 +40,6 @@ app = FastAPI(middleware=middleware)
 print("app loaded!")
 
 
-valid_params = {'models':['openai-community/gpt2'], 'tokenizers':['openai-community/gpt2']}
 
 @app.get("/")
 async def root():
